deee966d9460cb699b59a3ddf5081fceb253ca43
Jackodom <johncharlesodom@gmail.com>
2025-02-02 20:14:14 -0500

    Update testing-master.ino
    Fixed formatting

--- testing-full-master111.ino
+++ testing-full-master111.ino
@@ -17,9 +17,10 @@ SoftwareSerial nano5( 5,4 );
 SoftwareSerial nano6( 3,2 );
 SoftwareSerial* nanos[6] = { &nano2, &nano3, &nano1,  &nano4, &nano5, &nano6 };
 
-void visualOutput(){
+void visualOutput()
+{
   Serial.println(F("Checking Goal..."));
-        Serial.println(F("last goal detection."));
+  Serial.println(F("last goal detection."));
   char positions[12][18] = {
     { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
     { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
@@ -38,10 +39,12 @@ void visualOutput(){
   positions[(goalDistance / 150)][goalSensorCount] = 'x';
   Serial.println(F("           - - - - - - - - - - - - - - - - - - - - - - - - -Top Bar - - - - - - - - - - - - - - - - - - - - - - - - - - -"));
   Serial.print(F(" "));
-  for (int i = 0; i < 12; i++) {
+  for (int i = 0; i < 12; i++) 
+  {
     Serial.print((i + 1) * 6);
     Serial.print(" Inches:");
-    for (int j = 0; j < 18; j++) {
+    for (int j = 0; j < 18; j++) 
+    {
       Serial.print("|  ");
       Serial.print(positions[i][j]);
       Serial.print(" |");
@@ -52,25 +55,24 @@ void visualOutput(){
   while(true){}
 }
 
-  void send(String send){
+void send(String send)
+{
     int send_len = send.length() +1;
     char char_array[send_len];
     send.toCharArray(char_array, send_len);
     Serial.write(char_array, send_len);
-  }
+}
   void convertString(String rec, int i)
   {
     String letters = "ABCDEFGHIJ";
-
     int num = 0;
-
     num += letters.indexOf(rec.charAt(1)) * 1000;
     num += letters.indexOf(rec.charAt(2)) * 100;
     num += letters.indexOf(rec.charAt(3)) * 10;
     if(rec.charAt(0) == 'G')
     {
       Serial.println("goal");
-      goalSensorCount = (i) ;
+      goalSensorCount = (i+1) * 3;
       goalDistance = num;
       Serial.print(goalDistance);
       visualOutput();
@@ -80,37 +82,37 @@ void visualOutput(){
       interfereSensorCount = (i+1) * 3;
       interDistance = num;
       Serial.println("Interference");
-
     }
   }
 
-  void recieve(int i){
-  char input[6];
-    nanos[i]->readBytes(input,6);
-    
+  void recieve(int i)
+{
+  char input[4];
+  nanos[i]->readBytes(input,6);   
   String rec;
   rec = String(input);
-    char firstLetter = rec.charAt(0);
-    if(firstLetter == 'G' || firstLetter == 'I')
-    {
-      convertString(rec, i);
-    }
+  char firstLetter = rec.charAt(0);
+  if(firstLetter == 'G' || firstLetter == 'I')
+  {
+    convertString(rec, i);
+  }
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
   Serial.begin(115200);
-  //nano1.begin(115200);
+  nano1.begin(115200);
   nano2.begin(115200);
   nano3.begin(115200);
-  //nano4.begin(115200);
-  //nano5.begin(115200);
-  //nano6.begin(115200);
+  nano4.begin(115200);
+  nano5.begin(115200);
+  nano6.begin(115200);
 }
 
-void loop() {
-   for(int i = 0; i < 2; i++)
+void loop() 
+{
+   for(int i = 0; i < 6; i++)
    {
        recieve(i);
-  }
+   }
 }
