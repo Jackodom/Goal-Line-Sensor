765a16bdf534b078f429642e360f800aa1d6a31c
Jackodom <johncharlesodom@gmail.com>
2025-02-02 20:01:25 -0500

    Added files via upload
    Code for master & slave. Originally on local Arduino Uno and Nano.

---
+++ final-testing-slave.ino
@@ -0,0 +1,103 @@
+#include <VL53L1X.h>
+
+int distance = 0;
+bool starting = true;
+int averageDistance = 1800;
+
+const uint8_t sensorCount = 3;
+const uint8_t xshutPins[sensorCount] = { 2,3,4 };
+
+VL53L1X sensors[sensorCount];
+
+void clear(){
+  while(Serial.available() > 0)
+  {
+    Serial.read();
+  }
+}
+
+
+void initialize(){
+   for (uint8_t i = 0; i < sensorCount; i++) {
+    pinMode(xshutPins[i], OUTPUT);
+    digitalWrite(xshutPins[i], LOW);
+  }
+
+  // Enable, initialize, and start each sensor, one by one.
+  for (uint8_t i = 0; i < sensorCount; i++) {
+    pinMode(xshutPins[i], INPUT);
+    delay(10);
+
+    sensors[i].setTimeout(500);
+    if (!sensors[i].init()) {
+      Serial.print("Failed to detect and initialize sensor ");
+      Serial.println(i);
+    }
+    sensors[i].setAddress(0x2A + i);
+    sensors[i].setDistanceMode(VL53L1X::Short);
+    sensors[i].setROISize(4, 4);
+    sensors[i].setROICenter(199);
+    sensors[i].startContinuous(50);
+}
+}
+
+void analyzeReading(int i)  // Pass 'i' from the loop
+{
+  String cipher = "";
+  char letters[10] = {'A','B', 'C', 'D', 'E', 'F', 'G','H','I','J'};
+
+  long distance = sensors[i].read();  // Read distance for the current sensor
+  if ((distance + 150) < averageDistance)
+  {
+    delay(100);
+    long checkDistance = sensors[i].read();
+    if ((checkDistance + 150) < averageDistance) 
+    {
+      cipher += "I";
+    }
+    else
+    {
+      cipher += "G";
+    }
+
+    cipher += letters[(distance / 1000)];
+    cipher += letters[(distance % 1000) / 100];
+    cipher += letters[(distance % 100) / 10];
+
+    cipher += letters[i];
+
+    send(cipher);
+    //Serial.println(distance);
+  }
+}
+
+void recieve(){
+  char input[50];
+  while(Serial.available() > 0 )
+    {
+    Serial.readBytes(input,50);
+    }
+  String rec;
+  rec = String(input);
+  
+  char firstLetter = rec.charAt(0);
+}
+void send(String send){
+    int send_len = send.length() +1;
+    char char_array[send_len];
+    send.toCharArray(char_array, send_len);
+    Serial.write(char_array, send_len);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  initialize();
+}
+void loop() {
+  for (uint8_t i = 0; i < sensorCount; i++) 
+    { // Read the distance for each sensor
+      analyzeReading(i);
+    }
+}
+
---
+++ testing-full-master111.ino
@@ -0,0 +1,116 @@
+#include <SoftwareSerial.h>
+bool confirm = false;
+bool starting = true;
+int distance = 0;
+int goalSensorCount = 0;
+int interfereSensorCount = 0;
+int interDistance = 0;
+int goalDistance = 0;
+int sensor = 0;
+int processor = 0;
+
+SoftwareSerial nano1( 6,7 );
+SoftwareSerial nano2( 8, 9);
+SoftwareSerial nano3( 10,11 );
+SoftwareSerial nano4( 12, 13);
+SoftwareSerial nano5( 5,4 );
+SoftwareSerial nano6( 3,2 );
+SoftwareSerial* nanos[6] = { &nano2, &nano3, &nano1,  &nano4, &nano5, &nano6 };
+
+void visualOutput(){
+  Serial.println(F("Checking Goal..."));
+        Serial.println(F("last goal detection."));
+  char positions[12][18] = {
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+    { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ', ' ',' ', ' ', ' '},
+  };
+  positions[(interDistance / 150)][interfereSensorCount] = 'O';
+  positions[(goalDistance / 150)][goalSensorCount] = 'x';
+  Serial.println(F("           - - - - - - - - - - - - - - - - - - - - - - - - -Top Bar - - - - - - - - - - - - - - - - - - - - - - - - - - -"));
+  Serial.print(F(" "));
+  for (int i = 0; i < 12; i++) {
+    Serial.print((i + 1) * 6);
+    Serial.print(" Inches:");
+    for (int j = 0; j < 18; j++) {
+      Serial.print("|  ");
+      Serial.print(positions[i][j]);
+      Serial.print(" |");
+    }
+    Serial.println(" ");
+  }
+  Serial.println(F("Press red button on uno to restart"));
+  while(true){}
+}
+
+  void send(String send){
+    int send_len = send.length() +1;
+    char char_array[send_len];
+    send.toCharArray(char_array, send_len);
+    Serial.write(char_array, send_len);
+  }
+  void convertString(String rec, int i)
+  {
+    String letters = "ABCDEFGHIJ";
+
+    int num = 0;
+
+    num += letters.indexOf(rec.charAt(1)) * 1000;
+    num += letters.indexOf(rec.charAt(2)) * 100;
+    num += letters.indexOf(rec.charAt(3)) * 10;
+    if(rec.charAt(0) == 'G')
+    {
+      Serial.println("goal");
+      goalSensorCount = (i) ;
+      goalDistance = num;
+      Serial.print(goalDistance);
+      visualOutput();
+    }
+    else if(rec.charAt(0) == 'I')
+    {
+      interfereSensorCount = (i+1) * 3;
+      interDistance = num;
+      Serial.println("Interference");
+
+    }
+  }
+
+  void recieve(int i){
+  char input[6];
+    nanos[i]->readBytes(input,6);
+    
+  String rec;
+  rec = String(input);
+    char firstLetter = rec.charAt(0);
+    if(firstLetter == 'G' || firstLetter == 'I')
+    {
+      convertString(rec, i);
+    }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  //nano1.begin(115200);
+  nano2.begin(115200);
+  nano3.begin(115200);
+  //nano4.begin(115200);
+  //nano5.begin(115200);
+  //nano6.begin(115200);
+}
+
+void loop() {
+   for(int i = 0; i < 2; i++)
+   {
+       recieve(i);
+  }
+}
