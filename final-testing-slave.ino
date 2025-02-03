b1b0764f00cfc7ec5ecf1096a64629a63bb485d3
Jackodom <johncharlesodom@gmail.com>
2025-02-02 20:09:54 -0500

    Update final-testing-slave.ino
    Fixed Formatting

--- final-testing-slave.ino
+++ final-testing-slave.ino
@@ -40,63 +40,62 @@ void initialize(){
     sensors[i].startContinuous(50);
 }
 }
-
 void analyzeReading(int i)  // Pass 'i' from the loop
 {
   String cipher = "";
   char letters[10] = {'A','B', 'C', 'D', 'E', 'F', 'G','H','I','J'};
 
   long distance = sensors[i].read();  // Read distance for the current sensor
-  if ((distance + 150) < averageDistance)
-  {
-    delay(100);
-    long checkDistance = sensors[i].read();
-    if ((checkDistance + 150) < averageDistance) 
+    if ((distance + 150) < averageDistance)
     {
-      cipher += "I";
+      delay(100);
+      long checkDistance = sensors[i].read();
+      if ((checkDistance + 150) < averageDistance) 
+      {
+        cipher += "I";
+      }
+      else
+      {
+        cipher += "G";
+      }
+      cipher += letters[(distance / 1000)];
+      cipher += letters[(distance % 1000) / 100];
+      cipher += letters[(distance % 100) / 10];
+      cipher += letters[i];
+
+      send(cipher);
     }
-    else
-    {
-      cipher += "G";
-    }
-
-    cipher += letters[(distance / 1000)];
-    cipher += letters[(distance % 1000) / 100];
-    cipher += letters[(distance % 100) / 10];
-
-    cipher += letters[i];
-
-    send(cipher);
-    //Serial.println(distance);
-  }
 }
 
-void recieve(){
+void recieve()
+{
   char input[50];
   while(Serial.available() > 0 )
     {
-    Serial.readBytes(input,50);
+      Serial.readBytes(input,50);
     }
   String rec;
   rec = String(input);
-  
   char firstLetter = rec.charAt(0);
 }
-void send(String send){
+
+void send(String send)
+{
     int send_len = send.length() +1;
     char char_array[send_len];
     send.toCharArray(char_array, send_len);
     Serial.write(char_array, send_len);
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
   Serial.begin(115200);
   initialize();
 }
+
 void loop() {
   for (uint8_t i = 0; i < sensorCount; i++) 
-    { // Read the distance for each sensor
+    { 
       analyzeReading(i);
     }
 }
